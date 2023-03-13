a6c222cfa2fe1edc59722284224c34abba35daf4
Esfekia <sertac@gmail.com>
2023-03-13 07:01:13 -0400

    Towers now shoot bolts which damage enemies. Enemies have health system.

---
+++ Assets/_Core/Scripts/EnemyHealth.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyHealth : MonoBehaviour
+{
+
+    [SerializeField] int maxHitpoints = 5;
+    int currentHitpoints = 0;
+    int damage = 1;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentHitpoints = maxHitpoints;
+    }
+
+    void OnParticleCollision(GameObject other) 
+    {
+        ProcessHit();
+    }
+    
+    void ProcessHit()
+    {
+        currentHitpoints -= damage;
+        if (currentHitpoints <= 0) 
+        {
+            Destroy(gameObject);
+        }
+    }
+}
