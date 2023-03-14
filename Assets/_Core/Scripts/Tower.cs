82dd36ab4e2c42a4377e2aa6dd3103baef6e079b
Esfekia <sertac@gmail.com>
2023-03-14 09:26:28 -0400

    Towers cost gold to place. Game ends (restarts) when the player's bank balance is less than 0.

---
+++ Assets/_Core/Scripts/Tower.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tower : MonoBehaviour
+{
+    [SerializeField] int towerCost = 75;
+
+    public bool CreateTower(Tower tower, Vector3 position)
+    {
+        Bank bank = FindAnyObjectByType<Bank>();
+
+        if (bank == null)
+        {
+            return false;
+        }
+
+        if (bank.CurrentBalance >= towerCost)
+        {
+            Instantiate(tower, position, Quaternion.identity);
+            bank.Withdraw(towerCost);
+            return true;
+        }
+
+        return false;
+
+    }
+
+}
