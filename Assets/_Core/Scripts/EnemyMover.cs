91625eedf17e7e24ae450555f33e64fde9c4b161
Esfekia <sertac@gmail.com>
2023-03-16 12:15:38 -0400

    Enemies now follow the path as it changes, but the changes are only registered at newly created ones. Existing ones do not respect new obstacles.

--- Assets/_Core/Scripts/EnemyMover.cs
+++ Assets/_Core/Scripts/EnemyMover.cs
@@ -6,10 +6,15 @@ using UnityEngine;
 [RequireComponent(typeof(Enemy))]
 
 public class EnemyMover : MonoBehaviour
-{
-    [SerializeField] List<Tile> path = new List<Tile>();
+{    
     [SerializeField] [Range(0f, 5f)] float enemySpeed = 1f;
+    
+    List<Node> path = new List<Node>();
+    
     Enemy enemy;
+
+    GridManager gridManager;
+    Pathfinder pathfinder;
 
     // Start is called before the first frame update
     void OnEnable()
@@ -19,32 +24,23 @@ public class EnemyMover : MonoBehaviour
         StartCoroutine(FollowPath());        
     }
 
-    private void Start()
+    private void Awake()
     {
         enemy = GetComponent<Enemy>();
+        gridManager = FindObjectOfType<GridManager>();
+        pathfinder = FindObjectOfType<Pathfinder>();
     }
 
     void FindPath()
     {
         path.Clear();
-        
-        GameObject parent = GameObject.FindGameObjectWithTag("Path");
-
-        foreach (Transform child in parent.transform)
-        {
-            Tile waypoint = child.GetComponent<Tile>();
-            if (waypoint != null)
-            {
-                path.Add(waypoint);
-            }
-            
-        }
+        path = pathfinder.GetNewPath();
 
     }
 
     void ReturnToStart()
     {
-        transform.position = path[0].transform.position;
+        transform.position = gridManager.GetPositionFromCoordinates(pathfinder.StartCoordinates);
     }
 
     void FinishPath()
@@ -54,13 +50,10 @@ public class EnemyMover : MonoBehaviour
     }
     IEnumerator FollowPath()
     {
-        foreach(Tile waypoint in path)
-        {
-            //transform.position = waypoint.transform.position;
-            //yield return new WaitForSeconds(waitTime);
-
+        for(int i=0; i < path.Count; i++)
+        {       
             Vector3 startPosition = transform.position;
-            Vector3 endPosition = waypoint.transform.position;
+            Vector3 endPosition = gridManager.GetPositionFromCoordinates(path[i].coordinates);
             float travelPercent = 0f;
             transform.LookAt(endPosition);
             while (travelPercent <1)
